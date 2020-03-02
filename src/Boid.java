cf7cfa242fa4d11479c4e59b27d48c1325a2be65
WaIkn <frichetpierre98@gmail.com>
2020-03-02 17:37:30 +0100

    added Boid constructor and attributes

---
+++ src/Boid.java
@@ -0,0 +1,15 @@
+
+
+public class Boid{
+
+    private float[] position;
+    private float[] direction;
+    private static float speed;
+
+    public Boid(float[] position, float[] direction, float speed){
+        this.position = position;
+        this.direction = direction;
+        this.speed = speed;
+    }
+
+}
